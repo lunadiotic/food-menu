4363236df070d4c50b69ccb960a00bc7f003ec0d
aimeliala <idstack@outlook.co.id>
2023-06-27 16:07:10 +0700

    membuat banyak component dan mengaturnya

--- src/index.js
+++ src/index.js
@@ -4,7 +4,21 @@ import ReactDOM from "react-dom/client";
 function App() {
   return (
     <div>
-      <h1>Hello, React</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Warteg Mang Udin</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>Menu Kita</h2>
       <Food />
       <Food />
       <Food />
@@ -13,10 +27,14 @@ function App() {
   );
 }
 
+function Footer() {
+  return <footer>{new Date().getFullYear()} Warung Mang Udin</footer>;
+}
+
 function Food() {
   return (
     <div>
-      <img src="food/soto-betawi.jpg" alt="" />
+      <img src="food/soto-betawi.jpg" alt="" width="100" height="70" />
       <h2>Soto Betawi</h2>
       <p>Soto betawi dari Jakarta</p>
     </div>
